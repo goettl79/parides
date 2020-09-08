111e29c5c09832ffc269a259eef465505836a354
Georg Öttl <georg.oettl@dynatrace.com>
2020-09-08 22:21:09 +0200

    docs: update documentation examples

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ from parides.prom_conv import from_prom_to_df
 df = from_prom_to_df(
     resolution="15s",
     url="http://192.168.1.114:9090",
-    metrics_query="prometheus_engine_query_duration_seconds{quantile=\"0.99\"}"
+    metrics_query="irate(node_cpu_seconds_total{instance=\"192.168.1.114:9100\"}[5m]) * 100"
 )
-
-df.plot()
+plot = df.plot()
+plot.get_legend().remove()
 pyplot.show()
